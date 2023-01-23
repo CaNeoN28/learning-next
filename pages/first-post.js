0595952f5436e725901491632fd835de6060a5c1
carlos.steinheuser <carlosfelipe.st28@gmail.com>
2023-01-23 17:52:10 -0400

    Redirecionando para o primeiro post "

--- pages/first-post.js
+++ pages/first-post.js
@@ -1,3 +1,14 @@
+import Link from "next/link";
+
 export default function FirstPost(){
-    return <h1>Primeiro Post</h1>
+    return (
+        <>
+            <h1>
+                Primeiro Post
+            </h1>
+            <h2>
+                <Link href="/">Retornar ao inicio</Link>
+            </h2>
+        </>
+    )
 }
