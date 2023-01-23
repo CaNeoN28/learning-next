bdad5a09d8586e0a2d88f507eb46d293d738a09b
carlos.steinheuser <carlosfelipe.st28@gmail.com>
2023-01-23 19:55:29 -0400

    Usando um script externo

--- pages/first-post.js
+++ pages/first-post.js
@@ -1,17 +1,28 @@
 import Head from "next/head";
 import Image from "next/image";
 import Link from "next/link";
+import Script from "next/script";
 
 export default function FirstPost() {
   return (
     <>
       <Head>
         <title>Primeiro post</title>
+        <Script
+          src="https://connect.facebook.net/en_US/sdk.js"
+          strategy="lazyOnload"
+          onLoad={
+            console.log("O plugin do facebook foi carregado com sucesso")
+          }
+        />
       </Head>
+
+
       <Image
         src="/images/profile.png"
         width={100}
         height={100}
+        alt="Meu perfil"
       />
       <h1>
         Primeiro Post
