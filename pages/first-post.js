393814c9909564a0814fd246211792ac4e7c28fb
carlos.steinheuser <carlosfelipe.st28@gmail.com>
2023-01-23 19:48:33 -0400

    Adicionando imagem ao primeiro post

--- pages/first-post.js
+++ pages/first-post.js
@@ -1,14 +1,20 @@
+import Image from "next/image";
 import Link from "next/link";
 
-export default function FirstPost(){
-    return (
-        <>
-            <h1>
-                Primeiro Post
-            </h1>
-            <h2>
-                <Link href="/">Retornar ao inicio</Link>
-            </h2>
-        </>
-    )
+export default function FirstPost() {
+  return (
+    <>
+      <Image
+        src="/images/profile.png"
+        width={100}
+        height={100}
+      />
+      <h1>
+        Primeiro Post
+      </h1>
+      <h2>
+        <Link href="/">Retornar ao inicio</Link>
+      </h2>
+    </>
+  )
 }
