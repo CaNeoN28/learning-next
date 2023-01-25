ece0577069dfdd90943e730b7645da6a1d746c96
carlos.steinheuser <carlosfelipe.st28@gmail.com>
2023-01-25 16:31:19 -0400

    Atribuindo dados dos markdown à página

--- pages/index.js
+++ pages/index.js
@@ -1,8 +1,19 @@
 import Head from 'next/head';
 import Layout, { siteTitle } from '../components/layout';
+import { getSortedPostsData } from '../lib/post';
 import utilStyles from '../styles/utils.module.scss';
 
-export default function Home() {
+export async function getStaticProps(){
+  const allPostsData = getSortedPostsData();
+
+  return {
+    props: {
+      allPostsData
+    }
+  }
+}
+
+export default function Home({allPostsData}) {
   return (
     <Layout home>
       <Head>
@@ -10,6 +21,21 @@ export default function Home() {
       </Head>
       <section className={utilStyles.headingMd}>
         <p>Olá, eu sou Carlos Felipe, técnico em informática e estudante em análise e desenvolvimento de sistemas.</p>
+      </section>
+
+      <section className={`${utilStyles.headingMd} ${utilStyles.padding1px}`}>
+        <h2 className={utilStyles.headingLg}>Blog</h2>
+        <ul className={utilStyles.list}>
+          {allPostsData.map(({ id, date, title }) => (
+            <li className={utilStyles.listItem} key={id}>
+              {title}
+              <br />
+              {id}
+              <br />
+              {date}
+            </li>
+          ))}
+        </ul>
       </section>
     </Layout>
   );
